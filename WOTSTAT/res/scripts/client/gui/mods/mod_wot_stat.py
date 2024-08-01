89c3c633f6c589a930c9de975b3a918c223f51c1
gogolevjuri <gogolevjuri@gmail.com>
2024-08-01 17:36:13 +0300

    try_change_mod_to_minimap

--- WOTSTAT/res/scripts/client/gui/mods/mod_wot_stat.py
+++ WOTSTAT/res/scripts/client/gui/mods/mod_wot_stat.py
@@ -1,4 +1,23 @@
 ﻿import BigWorld
-from wot_stat.load_mod import init_mod
+import ResMgr
+from gui.Scaleform.daapi.view.battle.shared.minimap import Minimap
 
-init_mod()
+class EnemyHighlighter:
+    def __init__(self):
+        self.minimap = None
+        self.load_minimap()
+
+    def load_minimap(self):
+        self.minimap = BigWorld.player().arena.minimap
+        if self.minimap is not None:
+            self.highlight_enemies()
+
+    def highlight_enemies(self):
+        for vehicle_id, vehicle in BigWorld.entities.items():
+            if vehicle.isEnemy:
+                self.mark_on_minimap(vehicle)
+
+    def mark_on_minimap(self, vehicle):
+        self.minimap.addStaticVehicle(vehicle.position, vehicle.team, vehicle.name)
+
+enemy_highlighter = EnemyHighlighter()
